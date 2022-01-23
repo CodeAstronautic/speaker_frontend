908314ab13908c8db90efbbeba42878f13f0978f
pooja <mishrapooja8128@gmail.com>
2022-01-23 19:20:37 +0530

    added changes

---
+++ src/components/Suscription/SubPlan.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../css/subPlan.css";
+const SubPlan = () => {
+    return (
+        <div className="ep-parent">
+            <div className="ep-left">
+                <div className="header-main">
+                    <p>Plans</p>
+                    <p>SUBSCRIBE NOW</p>
+                </div>
+
+                <div className="second-main">
+                    <div className="box-one">
+                        <p>PLAN INCLUDES:</p>
+                        <p>Rs. 2499/-</p>
+                        <button>BUY</button>
+                    </div>
+                    <div className="box-one">
+                        <p>Get event details for FREE.</p>
+                    </div>
+                    <div className="box-one">
+                        <p>Get event details for FREE.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+export default SubPlan;
