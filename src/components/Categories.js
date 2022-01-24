46c706a10ecda22ab5b560eb0242ac13be77d651
priyanshuhaldar007 <priyanshu1843@gmail.com>
2022-01-25 00:46:44 +0530

    categorydone

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./css/category.css";
+import "./css/category.css";
 export default function Categories() {
   return (
     <>
@@ -7,56 +7,60 @@ export default function Categories() {
         <p className="head1">CATEGORIES</p>
         <p className="desc">WIDE RANGE OF CATEGORIES FOR SPEAKERORE USERS</p>
         <div className="catcards">
-          <div className="c1">
-            <img
-              className="imag1"
-              src={require("./images/category.jpg")}
-              alt={""}
-            />
-            <p className="detail1">FILM & INDUSTRY</p>
+          <div className="catcards-section">
+            <div className="catcard">
+              <img
+                className="catimgs"
+                src={require("./images/category.jpg")}
+                alt={""}
+              />
+              <p className="catsub">FILM & INDUSTRY</p>
+            </div>
+            <div className="catcard">
+              <img
+                className="catimgs"
+                src={require("./images/category.jpg")}
+                alt={""}
+              />
+              <p className="catsub">FILM & INDUSTRY</p>
+            </div>
+            <div className="catcard">
+              <img
+                className="catimgs"
+                src={require("./images/category.jpg")}
+                alt={""}
+              />
+              <p className="catsub">FILM & INDUSTRY</p>
+            </div>
           </div>
-          <div className="c2">
-            <img
-              className="imag2"
-              src={require("./images/category.jpg")}
-              alt={""}
-            />
-            <p className="detail2">FILM & INDUSTRY</p>
-          </div>
-          <div className="c3">
-            <img
-              className="imag3"
-              src={require("./images/category.jpg")}
-              alt={""}
-            />
-            <p className="detail3">FILM & INDUSTRY</p>
-          </div>
-          <div className="c4">
-            <img
-              className="imag4"
-              src={require("./images/category.jpg")}
-              alt={""}
-            />
-            <p className="detail4">FILM & INDUSTRY</p>
-          </div>
-          <div className="c5">
-            <img
-              className="imag5"
-              src={require("./images/category.jpg")}
-              alt={""}
-            />
-            <p className="detail5">FILM & INDUSTRY</p>
-          </div>
-          <div className="c6">
-            <img
-              className="imag6"
-              src={require("./images/category.jpg")}
-              alt={""}
-            />
-            <p className="detail6">FILM & INDUSTRY</p>
+          <div className="catcards-section">
+            <div className="catcard">
+              <img
+                className="catimgs"
+                src={require("./images/category.jpg")}
+                alt={""}
+              />
+              <p className="catsub">FILM & INDUSTRY</p>
+            </div>
+            <div className="catcard">
+              <img
+                className="catimgs"
+                src={require("./images/category.jpg")}
+                alt={""}
+              />
+              <p className="catsub">FILM & INDUSTRY</p>
+            </div>
+            <div className="catcard">
+              <img
+                className="catimgs"
+                src={require("./images/category.jpg")}
+                alt={""}
+              />
+              <p className="catsub">FILM & INDUSTRY</p>
+            </div>
           </div>
         </div>
-        <p className="head2">which one are you?</p>
+        {/* <p className="head2">which one are you?</p>
         <div className="c7">
           <p className="sub1">SPEAKER</p>
           <p className="subh1">Get nearby event detail by SpeakerOre</p>
@@ -88,7 +92,7 @@ export default function Categories() {
             via SPEAKERORE.
           </p>
           <button className="check1"> CREATE AN EVENT</button>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- src/components/Firstpage.js
+++ src/components/Firstpage.js
@@ -14,7 +14,7 @@ export default function Firstpage() {
         </div>
       </div>
       <About /> 
-      {/* <Categories /> */}
+      <Categories />
       
     </>
   );
