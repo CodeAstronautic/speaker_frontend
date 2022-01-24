ab6412c3057af994394864a7d2ca02dcc4f37c05
priyanshuhaldar007 <priyanshu1843@gmail.com>
2022-01-24 19:57:09 +0530

    moderatordraft1

--- src/components/ModeratorProfile.js
+++ src/components/ModeratorProfile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./css/ModeratorProfile.css";
 import user from "./images/profile.png";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
 
 export default function ModeratorProfile() {
   return (
@@ -87,10 +88,176 @@ export default function ModeratorProfile() {
               </div>
             </div>
           </div>
-          <div style={{ marginLeft: "2rem" }}>
-            Your information is secured with SpeakerOre
+          <div style={{ marginLeft: "2rem", display: "flex" }}>
+            <span style={{ fontSize: "2rem" }}>
+              <AiOutlineSafetyCertificate />
+            </span>
+            <span
+              style={{
+                fontSize: "small",
+                marginLeft: "0.5rem",
+                fontWeight: "bold",
+              }}
+            >
+              Your information is secured with <br /> SpeakerOre
+            </span>
           </div>
-          
+          <div className="mp-lower">
+            <div className="mpl-right">
+              <div className="mpl-heading">
+                <p>Event Details</p>
+                <button>Show previous events hosted</button>
+              </div>
+              <div className="mpedit-section">
+                <div className="mpes-form">
+                  <p>Name of the event</p>
+                  <input
+                    type="text"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Date of the event</p>
+                  <input
+                    type="date"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Mode</p>
+                  <select
+                    className="mpes-input"
+                    style={{
+                      paddingTop: "0",
+                      paddingBottom: "0",
+                      color: "grey",
+                    }}
+                  >
+                    <option value="Online" selected>
+                      Choose Mode
+                    </option>
+                    <option value="Online">Online</option>
+                    <option value="Offline">Offline</option>
+                  </select>
+                  <p>Location</p>
+                  <input
+                    type="text"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Topic covered</p>
+                  <input
+                    type="text"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Audience Size</p>
+                  <input
+                    type="text"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Payment Terms</p>
+                  <select
+                    className="mpes-input"
+                    style={{
+                      paddingTop: "0",
+                      paddingBottom: "0",
+                      color: "grey",
+                    }}
+                  >
+                    {/* <option value="Online" selected>Choose Mode</option> */}
+                    <option value="Online" selected>
+                      Paid
+                    </option>
+                    <option value="Offline">Offline</option>
+                  </select>
+                </div>
+                <div className="mpes-form">
+                  <p>This event includes (in short):</p>
+                  <textarea
+                    rows="4"
+                    cols="46"
+                    placeholder="A small about the event that is going to be held for the speakers."
+                    style={{
+                      background: "#F5F5F5",
+                      borderRadius: "10px",
+                      padding: "0.5rem",
+                      marginBottom: "1rem",
+                    }}
+                    res
+                  ></textarea>
+                  <p>About (elaborate)</p>
+                  <textarea
+                    rows="5"
+                    cols="46"
+                    placeholder="A small about the event that is going to be held for the speakers."
+                    style={{
+                      borderRadius: "10px",
+                      padding: "0.5rem",
+                      marginBottom: "1rem",
+                    }}
+                    res
+                  ></textarea>
+                  <p>Contact Information:</p>
+                  <input
+                    type="number"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Session Type</p>
+                  <select
+                    className="mpes-input"
+                    style={{
+                      paddingTop: "0",
+                      paddingBottom: "0",
+                      color: "grey",
+                    }}
+                  >
+                    <option value="Online" selected>
+                      Choose Mode
+                    </option>
+                    <option value="Online" selected>
+                      Conference
+                    </option>
+                    <option value="Offline">Offline</option>
+                  </select>
+                  <p>Audience Type</p>
+                  <input
+                    type="text"
+                    className="mpes-input"
+                    placeholder="Write Here"
+                  />
+                  <p>Tags</p>
+                  <div className="tag-div">
+                    <input
+                      type="text"
+                      className="mpes-input"
+                      placeholder="first"
+                    />
+                    <button>ADD</button>
+                  </div>
+                  <ul className="tags-parent">
+                    <li>first</li>
+                    <li>second</li>
+                    <li>third</li>
+                    <li>fourth</li>
+                    <li>fifth</li>
+                  </ul>
+                </div>
+                {/* <div className="confirmation">
+                  <div className="con-upper">
+                  <input type="checkbox" style={{position:'initial'}} class="apply1" name="apply1"/>
+                  <div className="conu-text">
+                  <h4>Apply for SpeakerOre Exclusive</h4>
+                 
+                  </div>
+                  </div>
+                  <div className="con-lower">
+                    
+                  </div>
+                </div> */}
+              </div>
+            </div>
+          </div>
         </div>
         <div className="ep-right">
           <div className="eprcard">
