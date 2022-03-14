6d42c943d92efd005ace0e8d6600c4530f8ba978
mPooja-15 <mishrapooja8128@gmail.com>
2022-03-14 21:55:42 +0530

    added changes

--- src/components/Events/SingleEventPageExclusive.js
+++ src/components/Events/SingleEventPageExclusive.js
@@ -1,4 +1,4 @@
-import React ,{useState}from "react";
+import React, { useState } from "react";
 // import "./css/SingleEventPageExclusive.css";
 import {
   BsChevronLeft,
@@ -18,40 +18,41 @@ export default function SingleEventPageExclusive() {
   const [start_time, setstart_time] = useState("");
   const [about, setabout] = useState("");
   const [location, setlocation] = useState("");
-  const [mode, setMode] = useState()
-  const [topic, setTopic] = useState()
-  const [AudienceType, setAudienceType] = useState()
-  const [paymentTerm, setpaymentTerm] = useState()
-  const [eventIncludes, seteventIncludes] = useState()
-  const [contact, setcontact] = useState()
-  const [sessionType, setsessionType] = useState()
-  const [tags, settags] = useState()
-  const [isExclusive, setisExclusive] = useState()
+  const [mode, setMode] = useState();
+  const [topic, setTopic] = useState();
+  const [AudienceType, setAudienceType] = useState();
+  const [paymentTerm, setpaymentTerm] = useState();
+  const [eventIncludes, seteventIncludes] = useState();
+  const [contact, setcontact] = useState();
+  const [sessionType, setsessionType] = useState();
+  const [tags, settags] = useState();
+  const [isExclusive, setisExclusive] = useState();
 
   const navigate = useNavigate();
   const handleEventCreate = (e) => {
     e.preventDefault();
     const data = {
-      name: name,
+      name: "Ergonomic Fresh Shoes",
       street: "091 Willms Via",
       state: "Louisiana",
-      country: location,
-      start_time: start_time || "",
+      country: "United States of America",
+      start_time: "2020-06-07T02:32:39.292Z",
       end_time: "2021-09-01T04:49:42.144Z",
       email: "Colt36@hotmail.com",
-      about: about,
+      about:
+        "Corrupti officia doloremque rerum harum. Ad sit accusantium distinctio aut similique et rerum unde.",
       city: "Justinetown",
       postalCode: "56525",
-      website: '',
-      mode: mode,
-      topic: topic,
-      location: location,
-      paymentTerm: paymentTerm,
-      eventIncludes: eventIncludes,
-      contact: contact,
-      sessionType: sessionType,
-      AudienceType: AudienceType,
-      tags: tags,
+      mode: "offline",
+      location: "new",
+      topic: "okkk",
+      paymentTerm: "offline",
+      eventIncludes: "hjhdjfhsd",
+      contact: "95394539",
+      sessionType: "jhfdjhfd",
+      AudienceType: "AudienceType",
+      tags: "tags",
+      website: "florine.net",
       description:
         "Praesentium omnis et dolore omnis dolores. Tempora non ut rem. Aut et beatae quisquam. Non eius nobis et eum porro qui facilis possimus ab.",
       categories: [
@@ -59,13 +60,23 @@ export default function SingleEventPageExclusive() {
         "minus",
         "dolorum",
         "ipsa",
+        "nesciunt",
+        "sed",
+        "non",
+        "distinctio",
+        "debitis",
+        "aut",
+        "incidunt",
+        "reiciendis",
+        "qui",
       ],
     };
     axios
       .post(`${process.env.REACT_APP_URL}/events`, data, {
         headers: {
-          Authorization: `Bearer ${JSON.parse(localStorage.getItem("@token")).token
-            }`,
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("@token")).token
+          }`,
         },
       })
       .then((data) => {
@@ -179,145 +190,150 @@ export default function SingleEventPageExclusive() {
               </button>
             </div>
             <div className="mplower-body">
-             <form onSubmit={handleEventCreate}>
-              <div className="mpedit-section" style={{ columnGap: "0" }}>
-                <div className="mpes-form">
-                  <p>Full Name</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Phone Number</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Address</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Website</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Books Authored</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                </div>
-                <div className="mpes-form">
-                  <p>Linkedin Profile</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Youtube Profile</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Facebook Profile</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Twitter Profile</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                  <p>Instagram Profile</p>
-                  <input
-                    type="text"
-                    className="mpes-input see-forminput"
-                    placeholder="Write Here"
-                  />
-                </div>
-                <div className="mpes-form form-ta">
-                  <p>Problems you are helping the audience solve:</p>
-                  <textarea
-                    rows="5"
-                    cols="46"
-                    placeholder="Write here..."
-                  ></textarea>
-                  <p>
-                    Sample Keynote Speakers Video Link:*(Write Fresher if no
-                    experience yet)
-                  </p>
-                  <textarea
-                    rows="5"
-                    cols="46"
-                    placeholder="Write here..."
-                  ></textarea>
-                  <p>
-                    Previous speaking references and contact information:*
-                    (Write Fresher for no experience yet)
-                  </p>
-                  <textarea
-                    rows="5"
-                    cols="46"
-                    placeholder="Write here..."
-                  ></textarea>
-                  <p>
-                    Anything extra you want the organizer to know about you:
-                  </p>
-                  <textarea
-                    rows="5"
-                    cols="46"
-                    placeholder="Write here..."
-                  ></textarea>
-                </div>
-              </div>
-              <div className="confirmation">
-                <div className="con-upper">
-                  <input
-                    type="checkbox"
-                    style={{ position: "initial" }}
-                    className="apply1"
-                    name="apply1"
-                  />
-                  <div className="conu-text">
-                    <h4>Applied via SpeakerOre Exclusive </h4>
-                    <span style={{fontSize:'medium'}}>
-                    Providing a direct way to connect with the event manager via mail. Tip: Share whatever sample you have.
-                    </span>
+              <form onSubmit={handleEventCreate}>
+                <div className="mpedit-section" style={{ columnGap: "0" }}>
+                  <div className="mpes-form">
+                    <p>Full Name</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Phone Number</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Address</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Website</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Books Authored</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                  </div>
+                  <div className="mpes-form">
+                    <p>Linkedin Profile</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Youtube Profile</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Facebook Profile</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Twitter Profile</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                    <p>Instagram Profile</p>
+                    <input
+                      type="text"
+                      className="mpes-input see-forminput"
+                      placeholder="Write Here"
+                    />
+                  </div>
+                  <div className="mpes-form form-ta">
+                    <p>Problems you are helping the audience solve:</p>
+                    <textarea
+                      rows="5"
+                      cols="46"
+                      placeholder="Write here..."
+                    ></textarea>
+                    <p>
+                      Sample Keynote Speakers Video Link:*(Write Fresher if no
+                      experience yet)
+                    </p>
+                    <textarea
+                      rows="5"
+                      cols="46"
+                      placeholder="Write here..."
+                    ></textarea>
+                    <p>
+                      Previous speaking references and contact information:*
+                      (Write Fresher for no experience yet)
+                    </p>
+                    <textarea
+                      rows="5"
+                      cols="46"
+                      placeholder="Write here..."
+                    ></textarea>
+                    <p>
+                      Anything extra you want the organizer to know about you:
+                    </p>
+                    <textarea
+                      rows="5"
+                      cols="46"
+                      placeholder="Write here..."
+                    ></textarea>
                   </div>
                 </div>
-                <div className="sepe-middle">**It is not compulsory to apply for the above option, it’s just a pact that you are making with us that you will only be hiring through our platform.</div>
-                <div className="con-lower">
-                  <button
-                    className="con-lower-btn eprbtn2"
-                    style={{
-                      margin: "1rem",
-                      border: "1px solid #ffbf19",
-                      background: "none",
-                    }}
-                  >
-                    Cancel Event
-                  </button>
-                  <button
-                    className="con-lower-btn eprbtn1"
-                    style={{
-                      margin: "1rem",
-                    }}
-                  >
-                    Add Event
-                  </button>
+                <div className="confirmation">
+                  <div className="con-upper">
+                    <input
+                      type="checkbox"
+                      style={{ position: "initial" }}
+                      className="apply1"
+                      name="apply1"
+                    />
+                    <div className="conu-text">
+                      <h4>Applied via SpeakerOre Exclusive </h4>
+                      <span style={{ fontSize: "medium" }}>
+                        Providing a direct way to connect with the event manager
+                        via mail. Tip: Share whatever sample you have.
+                      </span>
+                    </div>
+                  </div>
+                  <div className="sepe-middle">
+                    **It is not compulsory to apply for the above option, it’s
+                    just a pact that you are making with us that you will only
+                    be hiring through our platform.
+                  </div>
+                  <div className="con-lower">
+                    <button
+                      className="con-lower-btn eprbtn2"
+                      style={{
+                        margin: "1rem",
+                        border: "1px solid #ffbf19",
+                        background: "none",
+                      }}
+                    >
+                      Cancel Event
+                    </button>
+                    <button
+                      className="con-lower-btn eprbtn1"
+                      style={{
+                        margin: "1rem",
+                      }}
+                    >
+                      Add Event
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
             </div>
           </div>
         </div>
@@ -947,7 +963,7 @@ export default function SingleEventPageExclusive() {
           </div>
         </div>
       </div>
-     <LoggedInSidebar />
+      <LoggedInSidebar />
     </div>
   );
 }
